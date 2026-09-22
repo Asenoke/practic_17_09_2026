fa68bcec9cc5d592bf88a100de070fe40b4aa226
Alexandr <asenoke5@gmail.com>
2026-09-22 15:50:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace practic_17_09_2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
